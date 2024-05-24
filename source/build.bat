3371299ee3639981df8466155f2e7b85818ecd8b
Programmist <niko4pok@gmail.com>
2024-05-24 14:05:30 +0300

    removed no-source files

--- source/build.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-pyinstaller --noconfirm --clean --log-level FATAL --onedir --name %name% --contents-directory "." --noconsole --icon "icon.ico" --add-data "icon.ico";"." --add-data "settings.ini";"." "main.py"
--- source/lint.bat
+++
@@ -1,2 +0,0 @@
-cls
-pylint --extension-pkg-whitelist PyQt6 --disable used-before-assignment --good-names centralWidget,closeEvent,keyPressEvent main.py
